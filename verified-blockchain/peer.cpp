f1b765e5b7f66e0765af1008dcb2e3a953ffb5e1
Kallol Borah <18071236+kallolborah@users.noreply.github.com>
2020-05-10 20:38:40 +0530

    added basic code for registering new users on network and authenticating their sign ins

--- verified-blockchain/peer.cpp
+++ verified-blockchain/peer.cpp
@@ -17,24 +17,48 @@ VERIFIEDBLOCKCHAIN_API int peer::signup(std::string& username, std::string& pwd)
 
 	dht::InfoHash userid = dht::InfoHash::get(username+pwd);
 
-	dht::crypto::PrivateKey private_key = dht::crypto::PrivateKey::generate();
+	std::vector<uint8_t> user{userid.begin(), userid.end()};
+
+	dht::crypto::PrivateKey private_key (user, pwd);
 
 	peer::public_key = private_key.getPublicKey();
 
-	//dht::crypto::Certificate cert = dht::crypto::Certificate::generate(private_key, userid.toString);
-
-	//dht::crypto::PrivateKey private_key = dht::crypto::PrivateKey::generate(cert.toString);
-
+	//encrypted userid has to be persisted as an asset on network, so put function below has to be wrapped inside a verified transaction
+	node::get_node().putSigned(userid.to_c_str(), peer::public_key.encrypt(user));
+		
 	return 1;
-
 }
 
 
 VERIFIEDBLOCKCHAIN_API int peer::signin(std::string& username, std::string& pwd) {
 
-	peer::logged = true;
+	dht::InfoHash userid = dht::InfoHash::get(username + pwd);
+	
+	std::vector<uint8_t> user{userid.begin(), userid.end()};
+
+	dht::crypto::PrivateKey private_key(user, pwd);
+
+	peer::public_key = private_key.getPublicKey();
+	
+	//get function below has to be wrapped inside a verified transaction as encrypted userid is persisted as an asset on network
+	node::get_node().get(userid.to_c_str(), [&](const std::vector<std::shared_ptr<dht::Value>>& values) {
 		
-	return 1;
+		std::vector<uint8_t> decrypted;
+
+		for (const auto& value : values) {
+			std::vector<uint8_t> val{ value->data.begin(), value->data.end() };
+			decrypted = private_key.decrypt(val);
+			if(user == decrypted){
+				peer::logged = true;
+				return 1;
+			}
+			else
+				return 0;
+		}
+
+	});
+
+	return 0;
 	
 }
 
